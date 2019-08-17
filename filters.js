419155f2cef89fc08f8e8e3ec28fd2c543632b0f
Gleison Batista Soares <gleison@take.net>
2019-08-17 16:45:02 -0300

    Update filter for tracking report

--- filters.js
+++ filters.js
@@ -1,19 +1,8 @@
 function filters() {
 
     const hasTitleLike = (block, pattern) => block['$title'].includes(pattern)
-    const hasTitleEquals = (block, pattern) => block['$title'] === pattern
-
-    const hasTitleNotLike = (block, pattern) => !block['$title'].includes(pattern)
-    const hasTitleNotEquals = (block, pattern) => block['$title'] !== pattern
-
     const hasEnteringActionLike = (block, pattern) => block['$enteringCustomActions'].filter(a => a['$title'].includes(pattern)).length !== 0
-
     const hasLeavingActionLike = (block, pattern) => block['$leavingCustomActions'].filter(a => a['$title'].includes(pattern)).length !== 0
-    
-    const hasUserInput = (block, pattern) => block["$contentActions"].filter(a => a["input"] && a["input"]["bypass"] === false).length > 0
-
-    const hasQuickReply = (block, pattern) => block["$inputSuggestions"].length > 0
-
     const hasMessageToUser = (block, pattern) => {
         for (contentAction of block["$contentActions"]) {
             action = contentAction["action"]
@@ -27,27 +16,28 @@ function filters() {
         }
         return false
     }
-
+    const hasUserInput = (block, pattern) => block["$contentActions"].filter(a => a["input"] && !a["input"]["bypass"]).length > 0
+    
+    const hasTrackings = (block, pattern) => {
+        const et = block["$enteringCustomActions"].filter(a => a["type"] === "TrackEvent")
+        const lt = block["$leavingCustomActions"].filter(a => a["type"] === "TrackEvent")
+        return et.length > 0 || lt.length > 0
+    }
+    
     const getByDescription = (description) => {
         const descriptionToFunctionMapping = {
-            "title like": hasTitleLike,
-            "title equals": hasTitleEquals,
-            "title not like": hasTitleNotLike,
-            "title not equals": hasTitleNotEquals,
+            "title contains": hasTitleLike,
             "entering action like": hasEnteringActionLike,
             "leaving action like": hasLeavingActionLike,
-            "sends message": hasMessageToUser,
-            "awaits input": hasUserInput,
-            "has quick reply": hasQuickReply
+            "has message to user": hasMessageToUser,
+            "has user input": hasUserInput,
+            "has trackings": hasTrackings
         }
     
         return descriptionToFunctionMapping[description]
     }
 
     function blockMatchesAll(block, criterias) {
-        if (block["id"] === "onboarding")
-            return false
-            
         for (criteria of criterias) {
             let filterFunction = getByDescription(criteria.description)
             if (!filterFunction(block, criteria.pattern)) {
