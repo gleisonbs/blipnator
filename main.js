2cf4312d33a7e14a93750f41ec8750f7a8db3006
Gleison Batista Soares <gleison@take.net>
2019-08-17 16:38:04 -0300

    Add report trackings feature

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const state = require('./state.js')
 const parameters = require('./actions/parameters.js')
 const filters = require('./filters.js')
+const report = require('./report.js')
 const actions = require('./actions/actions.js')
+
 
 const saveBot = (bot) => state().save(bot, './output/newbot.json')
 const loadBot = () => state().load(process.argv[2])
@@ -11,19 +13,41 @@ function start() {
     bot = loadBot()
     instructions = loadInstructions()
 
-    for (blockName of Object.keys(bot)) {
-        block = bot[blockName]
-        
-        for (operation of instructions.addOperations) {
-            if (filters().blockMatchesAll(block, operation.criterias))
-                for (action of operation.actions)
-                    actions().addToBlock(action, block, parameters.get(block))
+    for (operation of instructions.report) {
+        let totalBlocks = 0
+        let totalBlocksMatchingCriteria = 0
+        let totalBlocksHighlighted = 0
+        for (blockName of Object.keys(bot)) {
+            block = bot[blockName]
+            
+            if (filters().blockMatchesAll(block, operation.criterias)) {
+                totalBlocksMatchingCriteria += 1
+                const { hasHighlight, formattedOutput } = report(block, operation.highlight).display(operation.display)
+                console.log(formattedOutput)
+                if (hasHighlight)
+                    totalBlocksHighlighted += 1
+            }
+            totalBlocks += 1
         }
 
-        for (operation of instructions.removeOperations) {
-            console.log("Remove operations are not supported yet.")
-            break
+        console.log(`\n\nTotal blocks: ${totalBlocks}`)
+        console.log(`Total blocks matching criteria: ${totalBlocksMatchingCriteria}`)
+        console.log(`Total blocks highlighted: ${totalBlocksHighlighted}`)
+    }
+
+    for (operation of instructions.remove) {
+        for (blockName of Object.keys(bot)) {
+            block = bot[blockName]
         }
+    }
+
+    for (operation of instructions.add) {
+        for (blockName of Object.keys(bot)) {
+            block = bot[blockName]
+
+            if (filters().blockMatchesAll(block, operation.criterias))
+                operation.actions.forEach(a => actions().addToBlock(a, block, parameters.get(block)))    
+        }     
     }
 
     saveBot(bot)
---
+++ report.js
@@ -0,0 +1,90 @@
+function report(block, highlight) {
+    let hasHighlight = false
+
+    const cleanBlockTitle = () => {
+        const titleStart = block["$title"].indexOf("]")
+        const cleanTitle = block["$title"].substring(titleStart + 1).trim()
+        return cleanTitle
+    }
+
+    const cleanTrackingCategory = (tracking) => {
+        if (tracking["type"] !== "TrackEvent") 
+            return ""
+
+        const [cleanCategory, _] = tracking["settings"]["category"].split('-')
+        return cleanCategory.trim()
+    }
+
+    const getHighlight = (action) => {
+        const mappings = {
+            "tracking category different from block title": (action) => cleanTrackingCategory(action) !== "" && cleanBlockTitle().toLowerCase() !== cleanTrackingCategory(action).toLowerCase()
+        }
+
+        if (mappings[highlight](action))
+            hasHighlight = true
+    }
+
+    function display(actions) {
+        let formattedBlockTitle = `${block["$title"]}\n`
+        
+        const enteringActions = getEnteringProperties(actions)
+        const leavingActions = getLeavingProperties(actions)
+
+        const formattedEnteringActions = enteringActions ? `\tEntering Actions\n${enteringActions}` : ""
+        const formattedLeavingActions = leavingActions ? `\tLeaving Actions\n${leavingActions}` : ""
+
+        const formattedOutput = `${hasHighlight ? "--> " : ""}${formattedBlockTitle}${formattedEnteringActions}${formattedLeavingActions}`
+
+        return { hasHighlight, formattedOutput }
+    }
+
+    function getEnteringProperties(actions) {
+        const { trackings, scripts } = actions
+
+        let enteringTrackings = trackings ? getActions("entering", "TrackEvent") : ""
+        let enteringScripts = scripts ? getActions("entering", "ExecuteScript") : ""
+
+        return `${enteringTrackings}${enteringScripts}`
+    }
+
+    function getLeavingProperties(actions) {
+        const { trackings, scripts } = actions
+
+        let leavingTrackings = trackings ? getActions("leaving", "TrackEvent") : ""
+        let leavingScripts = scripts ? getActions("leaving", "ExecuteScript") : ""
+ 
+        return `${leavingTrackings}${leavingScripts}`
+    }
+    
+    function getActions(timing, type) {
+        actionTiming = timing === "leaving" ? "$leavingCustomActions" : "$enteringCustomActions"
+        const actions = block[actionTiming].filter(a => a["type"] === type)
+
+        let formattedActionOutput = ""
+        for (action of actions) {
+            if (type === "TrackEvent")
+                formattedActionOutput += getFormattedTracking(action)
+            else if (type === "ExecuteScript")
+                formattedActionOutput += getFormattedScript(action)
+        }
+
+        return formattedActionOutput
+    }
+
+    const getFormattedTracking = (tracking) => {      
+        getHighlight(tracking)
+        return `\t\tT+ ${tracking["settings"]["category"]}: ${tracking["settings"]["action"]}\n`
+    }
+
+    const getFormattedScript = (script) => {
+        getHighlight(script)
+        return `\t\tS+ ${script["$title"]}\n`
+    }
+
+    return {
+        display,
+        hasHighlight
+    }
+}
+
+module.exports = report
